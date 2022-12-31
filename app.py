d9ed2cb177662fa57828c0ed1862f8518b0b7877
Ilya <72245457+terrorChay@users.noreply.github.com>
2022-12-31 19:42:53 +0300

    added doctors

--- app.py
+++ app.py
@@ -7,41 +7,55 @@ import re
 from io import BytesIO
 
 regions = {
-    'Астрахань' : 'https://prodoctorov.ru/astrahan/lpu',
-    'Сочи'      : 'https://prodoctorov.ru/sochi/lpu',
-    'Тюмень'    : 'https://prodoctorov.ru/tyumen/lpu',
-    'Воронеж'   : 'https://prodoctorov.ru/voronezh/lpu',
+    'Астрахань' : 'https://prodoctorov.ru/astrahan/',
+    'Сочи'      : 'https://prodoctorov.ru/sochi/',
+    'Тюмень'    : 'https://prodoctorov.ru/tyumen/',
+    'Воронеж'   : 'https://prodoctorov.ru/voronezh/',
 }
 
 @st.experimental_memo
-def scrape(address):
+def scrape(address, find_lpu, page_limit):
     # Define things to find
-    target_attrs = {
-        'Название'      : ('span', {"data-qa": "lpu_card_heading_lpu_name"}),
-        'Тип'           : ('div', {"data-qa": "lpu_card_subheading_lputype_name"}),
-        'Кол-во врачей' : ('div', {"data-qa": "lpu_card_subheading_doctors_count"}),
-        'Адрес'         : ('span', {"data-qa": "lpu_card_btn_addr_text"}),
-        'Телефон'       : ('span', {"data-qa": "lpu_card_btn_phone_text"}),
-        'Открыто до'    : ('span', {"data-qa": "lpu_card_btn_schedule_text"}),
-        'Цены'          : ('span', {"data-qa": "lpu_card_btn_prices_num"}),
-        'Отзывы'        : ('span', {"data-qa": "lpu_card_stars_text"}),
-    }
+    if find_lpu:
+        target_attrs = {
+            'Название'      : ('span', {"data-qa": "lpu_card_heading_lpu_name"}),
+            'Тип'           : ('div', {"data-qa": "lpu_card_subheading_lputype_name"}),
+            'Кол-во врачей' : ('div', {"data-qa": "lpu_card_subheading_doctors_count"}),
+            'Адрес'         : ('span', {"data-qa": "lpu_card_btn_addr_text"}),
+            'Телефон'       : ('span', {"data-qa": "lpu_card_btn_phone_text"}),
+            'Открыто до'    : ('span', {"data-qa": "lpu_card_btn_schedule_text"}),
+            'Цены'          : ('span', {"data-qa": "lpu_card_btn_prices_num"}),
+            'Отзывы'        : ('span', {"data-qa": "lpu_card_stars_text"}),
+        }
+    else:
+        target_attrs = {
+            'ФИО'               : ('span', {"class": "b-doctor-card__name-surname"}),
+            'Специальность'     : ('div', {"class": "b-doctor-card__spec"}),
+            'Стаж'              : ('div', {"class": "b-doctor-card__experience-years"}),
+            'Категория'         : ('div', {"class": "b-doctor-card__category"}),
+            'Отзывов'           : ('a', {"class": "ui-text ui-text_body-2 b-link b-link_prg b-link_color_grey b-link_underline"}),
+            'Клиника'           : ('span', {"class": "b-select__trigger-main-text"}),
+            'Адрес клиники'     : ('span', {"class": "b-select__trigger-adit-text"}),
+        }
 
     # Output dataframe to fil
-    df = pd.DataFrame(columns=['Название', 'Тип', 'Кол-во врачей', 'Адрес', 'Телефон', 'Открыто до', 'Представлено цен', 'Отзывов'])
+    df = pd.DataFrame(columns=target_attrs.keys())
 
     page_num = 1
     response = '200'
-    while response == '200':
+    while response == '200' and page_num-1 != page_limit:
         url = f"{address}/?page={page_num}"
         page = requests.get(url)
         response = str(page.status_code)
         if response == '200':
             page_num += 1
             soup = BeautifulSoup(page.text, "html.parser")
-            # Iterate over clinic cards
-            allLPU = soup.findAll('div', class_='b-card__row')
-            for item in allLPU:
+            # Iterate over
+            if find_lpu:
+                all = soup.findAll('div', class_='b-card__row')
+            else:
+                all = soup.findAll('div', class_='b-doctor-card')
+            for item in all:
                 item_data = []
                 for key, attrs in target_attrs.items():
                     data_unit = item.find(*attrs)
@@ -49,11 +63,13 @@ def scrape(address):
                         raw_text = data_unit.text.strip("""\n               """)
                         if key == 'Кол-во врачей' or key == 'Отзывы':
                             raw_text = re.sub(r"\D", "", raw_text)
+                        elif key == 'Специальность':
+                            raw_text = ', '.join(list(map(lambda x: x.strip() , raw_text.split(','))))
                         item_data.append(raw_text)
                     else:
                         item_data.append(data_unit)
                 df.loc[len(df)] = item_data
-    st.success(f'Проанализировано {page_num-1} страниц! Страница {page_num} вернула ошибку {response}.')
+    st.success(f'Проанализировано {page_num-1} страниц! Страница {page_num} вернула код {response}.')
     return df
 
 @st.experimental_memo
@@ -75,11 +91,16 @@ def convert_df(df: pd.DataFrame, to_excel=False):
 def main():
     st.subheader('ilya@matyush.in')
     with st.form('parser'):
-        region = st.selectbox('Выберите регион', ['Астрахань', 'Сочи', 'Тюмень', 'Воронеж'])
+        region = st.selectbox('Где ищем?', ['Астрахань', 'Сочи', 'Тюмень', 'Воронеж'])
+        to_find = st.selectbox('Что ищем?', ['ЛПУ', 'Врачи'])
+        page_limit = st.select_slider('Максимум страниц', options=['Нет']+list(range(1,21)))
         address = regions[region]
         submit = st.form_submit_button('Поехали')
     if submit:
-        df = scrape(address)
+        if to_find == 'ЛПУ':
+            df = scrape(address+'lpu', True, page_limit)
+        else:
+            df = scrape(address+'vrach', False, page_limit)
         st.dataframe(df)
         st.download_button('💾 Excel', data=convert_df(df, True), file_name=f"{region}.xlsx")
 
